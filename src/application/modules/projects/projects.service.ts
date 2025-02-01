177fc5bcd47b2f91b5d8d6b2c5ce74c892d5b7d6
imnavi-py <alireza.navipy@gmail.com>
2025-02-01 05:35:19 -0800

    update projectservie

--- src/application/modules/projects/projects.service.ts
+++ src/application/modules/projects/projects.service.ts
@@ -1,26 +1,45 @@
 import { Injectable } from '@nestjs/common'
 import { CreateProjectDto } from './dto/create-project.dto'
 import { UpdateProjectDto } from './dto/update-project.dto'
+import { PrismaService } from '~/common/modules/prisma/prisma.service'
+import { Project } from '@prisma/client'
 
 @Injectable()
 export class ProjectsService {
-  create(createProjectDto: CreateProjectDto) {
-    return 'This action adds a new project'
+  constructor(private readonly prisma: PrismaService) {}
+  create(createProjectDto: CreateProjectDto): Promise<Project> {
+    return this.prisma.project.create({
+      data: createProjectDto
+    })
   }
 
-  findAll() {
-    return `This action returns all projects`
+  findAll(): Promise<Project[]> {
+    return this.prisma.project.findMany({
+      include: {
+        tasks: true
+      }
+    })
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} project`
+  findOne(id: number): Promise<Project> {
+    return this.prisma.project.findUniqueOrThrow({
+      where: { id },
+      include: {
+        tasks: true
+      }
+    })
   }
 
-  update(id: number, updateProjectDto: UpdateProjectDto) {
-    return `This action updates a #${id} project`
+  update(id: number, updateProjectDto: UpdateProjectDto): Promise<Project> {
+    return this.prisma.project.update({
+      where: { id },
+      data: updateProjectDto
+    })
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} project`
+  remove(id: number): Promise<Project> {
+    return this.prisma.project.delete({
+      where: { id }
+    })
   }
 }
