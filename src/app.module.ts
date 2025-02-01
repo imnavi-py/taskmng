53e3ceb1cfe23485cf64062a74829c1eae5a86c0
imnavi-py <alireza.navipy@gmail.com>
2025-02-01 05:16:16 -0800

    update prisma-config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { TasksModule } from './application/modules/tasks/tasks.module'
 import { ProjectsModule } from './application/modules/projects/projects.module'
+import { PrismaModule } from './common/modules/prisma/prisma.module'
 
 @Module({
-  imports: [TasksModule, ProjectsModule]
+  imports: [TasksModule, ProjectsModule, PrismaModule]
 })
 export class AppModule {}
--- src/application/modules/projects/dto/create-project.dto.ts
+++ src/application/modules/projects/dto/create-project.dto.ts
@@ -1,1 +1,10 @@
-export class CreateProjectDto {}
+import { IsEnum, IsString } from 'class-validator'
+import { ProjectStatusEnum } from '@prisma/client'
+
+export class CreateProjectDto {
+  @IsString()
+  name: string
+
+  @IsEnum(ProjectStatusEnum)
+  status: ProjectStatusEnum
+}
