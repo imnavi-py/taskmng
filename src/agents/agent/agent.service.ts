f7e31f12a31e2872486e4dd767b26f1781b7d3be
Aliezzanvi <127602696+imnavi-py@users.noreply.github.com>
2025-02-07 12:16:57 -0800

    fixed agent errors

--- src/agents/agent/agent.service.ts
+++ src/agents/agent/agent.service.ts
@@ -2,7 +2,7 @@ import { BadRequestException, Injectable, NotFoundException } from '@nestjs/comm
 import { HttpService } from '@nestjs/axios'
 import { CreateAgentDto } from './dto/create-agent.dto'
 import { PrismaService } from '~/common/modules/prisma/prisma.service'
-import { Agent } from '@prisma/client'
+import { Agent, workStatusEnum } from '@prisma/client'
 
 @Injectable()
 export class AgentService {
@@ -24,19 +24,15 @@ export class AgentService {
     try {
       const response = await this.httpService.axiosRef.post(data.webhook, queryParams)
 
-      if (response.status !== 200) {
-        await this.updateAgentStatus(agent.id, 'faild')
-        throw new Error('Webhook responded with a non-200 status code')
+      if (response.status === 200) {
+        await this.updateAgentStatus(agent.id, workStatusEnum.ok)
       }
-
-      await this.updateAgentStatus(agent.id, 'ok')
-      return agent
-    } catch (error) {
-      await this.updateAgentStatus(agent.id, 'faild')
-      if (error instanceof NotFoundException) {
-        throw error
-      }
+    } catch {
+      await this.updateAgentStatus(agent.id, workStatusEnum.faild)
+      throw new BadRequestException('Failed to send data to webhook: Webhook not reachable')
     }
+
+    return agent
   }
 
   async findById(id: number): Promise<Omit<Agent, 'webhook'>> {
@@ -55,7 +51,7 @@ export class AgentService {
     return agents.map(({ webhook, ...result }) => result)
   }
 
-  private async updateAgentStatus(agentId: number, status: 'ok' | 'faild') {
+  private async updateAgentStatus(agentId: number, status: workStatusEnum) {
     return this.prisma.agent.update({
       where: { id: agentId },
       data: { status: status }
